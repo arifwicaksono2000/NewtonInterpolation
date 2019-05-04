f3af78e122ed837e3cbdc14c136504c3879c9284
marbelbel0 <marbelbel0@gmail.com>
2019-05-05 05:11:10 +0700

    first commit

---
+++ newtonInterpolation/CountingTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newtonInterpolation
+{
+    class CountingTest
+    {
+        public float inter2_1;
+        public float inter2_2;
+        public float inter2_3;
+        public float inter3_1;
+        public float inter3_2;
+        public float inter4_1;
+        
+
+        public CountingTest(float number1, float number2, float number3, float number4)
+        {
+
+            inter2_1 = number2 - ((number2 - number1) / 3);
+            inter2_2 = number3 - ((number3 - number2) / 3);
+            inter2_3 = number4 - ((number4 - number3) / 3);
+
+            inter3_1 = inter2_2 - ((inter2_2 - inter2_1) / 3);
+            inter3_1 = inter2_2 - ((inter2_2 - inter2_1) / 3);
+            inter3_1 = inter2_2 - ((inter2_2 - inter2_1) / 3);
+            inter3_1 = inter2_2 - ((inter2_2 - inter2_1) / 3);
+            inter3_2 = inter2_3 - ((inter2_3 - inter2_2) / 3);
+
+            inter4_1 = inter3_2 - ((inter3_2 - inter3_1) / 3);
+           
+        }
+    }
+}
---
+++ newtonInterpolation/Form2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace newtonInterpolation
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHitung_Click(object sender, EventArgs e)
+        {
+            float Input1 = float.Parse(Inter1_1.Text);
+            float Input2 = float.Parse(Inter1_2.Text);
+            float Input3 = float.Parse(Inter1_3.Text);
+            float Input4 = float.Parse(Inter1_4.Text);
+            //Input1 = Input1 + Input2 + Input3 + Input4;
+            //string Input = Input1.ToString();
+            CountingTest counting = new CountingTest(Input1, Input2, Input3, Input4);
+            //bool test = counting.loadScenarioNumberExecuted;
+            //if (test)
+            //{ MessageBox.Show("Lol"); }
+            Inter2_1.Text = counting.inter2_1.ToString();
+            Inter2_2.Text = counting.inter2_2.ToString();
+            Inter2_3.Text = counting.inter2_3.ToString();
+            Inter3_1.Text = counting.inter3_1.ToString();
+            Inter3_2.Text = counting.inter3_2.ToString();
+            Inter4_1.Text = counting.inter4_1.ToString();
+            //Inter4_1.Text = Input;
+
+        }
+    }
+}
